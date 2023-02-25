5fd8cd6a3b9bc5ad1326cf313c4681db9e0af34f
Alex Arias <alex.arias.jorym@gmail.com>
2023-02-25 13:09:16 +0800

    feat: rename web app project and add reset CSS and header implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './Assets/Styles/app.css';
 import Header from './Layouts/Header/Header';
 import Main from './Layouts/Main/Main';
 import Footer from './Layouts/Footer/Footer';
--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      Fuck
+      <h1>Anonymyst</h1>
     </header>
   )
 }
