3c4f87f1791fd360d55404d8ee588b9ea83dd7ea
Alex Arias <alex.arias.jorym@gmail.com>
2023-02-25 15:06:47 +0800

    feat: implement web app footer and make the app more responsive for mobile devices

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../../Assets/Styles/footer.css'
 
 const Footer = () => {
   return (
     <footer>
-      
+      <p className='text-sm'>Built by Yugen</p>
+      <p className='text-xsm'>For entertainment purposes only.</p>
     </footer>
   )
 }
