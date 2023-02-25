c73d899e2a28933c533ab9d8cc88ce2b21501b16
Alex Arias <alex.arias.jorym@gmail.com>
2023-02-25 16:03:57 +0800

    feat: implement responsive menu/navigation bar

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import { useRef } from 'react';
 import '../../Assets/Styles/header.css';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import { FaPen, FaSearch } from "react-icons/fa";
 import { AiFillHome } from "react-icons/ai";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("nav-open");
+  }
+
   return (
     <header>
       <h1>Anonymyst</h1>
-      <nav>
-        <i className='nav-icon'><AiFillHome size={23}  color='white' /></i>
-        <i className='nav-icon'><FaPen size={19} color='white' /></i>
-        <i className='nav-icon'><FaSearch size={20}  color='white' /></i>
-        <button className='btn'>Sign Up</button>
+      <nav className='menu'>
+        <i className='nav-icon'><AiFillHome size={23}  color='white' title='Home' /></i>
+        <i className='nav-icon'><FaPen size={19} color='white' title='Write' /></i>
+        <i className='nav-icon'><FaSearch size={20}  color='white' title='Search' /></i>
+        <i className='btn'>Sign Up</i>
       </nav>
+      <button className='nav-btn' onClick={showNavbar}>
+        <HiOutlineMenuAlt3 size={23} color='white' />
+      </button>
+      <div className='nav-dropdown' ref={navRef}>
+        <i className='nav-icon menu-item'><AiFillHome size={23}  color='white' title='Home' /><p>Home</p></i>
+        <i className='nav-icon menu-item'><FaPen size={19} color='white' title='Write' /><p>Write</p></i>
+        <i className='nav-icon menu-item'><FaSearch size={20}  color='white' title='Search' /><p>Search</p></i>
+        <i className='btn menu-item'><p>Sign Up</p></i>
+      </div>
     </header>
   )
 }
