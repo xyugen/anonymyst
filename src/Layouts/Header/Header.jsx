c4fd5c3309ad164fb7a81eda762f68bc8cbba651
Alex Arias <alex.arias.jorym@gmail.com>
2023-02-25 14:32:28 +0800

    feat: add react-icons and header styling with icons, rename project to "anonymyst"

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import '../../Assets/Styles/header.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { FaPen, FaSearch } from "react-icons/fa";
+import { AiFillHome } from "react-icons/ai";
 
 const Header = () => {
   return (
     <header>
       <h1>Anonymyst</h1>
+      <nav>
+        <i className='nav-icon'><AiFillHome size={23}  color='white' /></i>
+        <i className='nav-icon'><FaPen size={19} color='white' /></i>
+        <i className='nav-icon'><FaSearch size={20}  color='white' /></i>
+        <button className='btn'>Sign Up</button>
+      </nav>
     </header>
   )
 }
--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Main = () => {
   return (
     <main>
-      
+      test
     </main>
   )
 }
-
+/*
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
+*/
 export default Main
