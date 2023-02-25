98aa701fb784b17d5bda32628cd5868f4a2a776a
Alex Arias <alex.arias.jorym@gmail.com>
2023-02-25 22:50:56 +0800

    feat: created a page for users to create posts and store them in Supabase database.

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../../Assets/Styles/post.css'
+import { useNavigate } from 'react-router-dom'
+
+import { supabase } from '../../Services/supabase'
 
 const Post = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const history = useNavigate();
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Insert the new post into the "posts" table
+    const { data, error } = await supabase
+      .from('posts')
+      .insert([{ title, content }]);
+
+    if (error) {
+      console.log('Error creating post:', error);
+    } else {
+      console.log('Post created successfully:', data);
+    }
+
+    history("/")
+  };
+
   return (
     <div className='post-form'>
       <div className='post-form-header'>
-        <p>Write</p><h2>Anonymyst</h2>
+        <h2>Anonymyst</h2>
       </div>
-      <div className='post-form-content'>
-        <form>
-          <label>Title: </label>
-          <input type='text' />
+      <div className='post-form-content' onSubmit={handleSubmit}>
+        <form className='post-form-form'>
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" value={title} onChange={handleTitleChange} />
+
+          <label htmlFor="content">Content:</label>
+          <textarea id="content" value={content} onChange={handleContentChange} required></textarea>
+
+          <input type="submit" value="Post" className='btn' id='post-btn' />
         </form>
       </div>
     </div>
