36f92e09566be988ac66553549a139efd50593be
Alex Arias <alex.arias.jorym@gmail.com>
2023-03-18 16:52:52 +0800

    feat: add image upload feature for posts

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
-import '../../Assets/Styles/post.css';
-import { useNavigate } from 'react-router-dom';
-import Dialog from '../../Components/DialogBox/Dialog';
+import React, { useState } from "react";
+import "../../Assets/Styles/post.css";
+import { useNavigate } from "react-router-dom";
+import Dialog from "../../Components/DialogBox/Dialog";
 
-import { supabase } from '../../Services/supabase';
+import { supabase } from "../../Services/supabase";
 
 const Post = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const MAX_FILE_SIZE = 8000000;
   const history = useNavigate();
 
   const handleTitleChange = (e) => {
@@ -19,6 +22,24 @@ const Post = () => {
     setContent(e.target.value);
   };
 
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    // Check file size
+    if (file.size > MAX_FILE_SIZE) {
+      // 8MB limit
+      setErrorMessage("File size cannot exceed 8MB.");
+      return;
+    }
+
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -26,23 +47,77 @@ const Post = () => {
     const newContent = content.trim();
 
     // Check if content is empty
-    if (newContent === '') {
-      setErrorMessage('Content cannot be blank.');
+    if (newContent === "") {
+      setErrorMessage("Content cannot be blank.");
+      return;
+    }
+
+    // Check if image is uploaded and its size is valid
+    if (!image || image.size > MAX_FILE_SIZE) {
+      setErrorMessage(
+        `Image must be less than ${MAX_FILE_SIZE / (1024 * 1024)} MB.`
+      );
       return;
     }
 
     // Insert the new post into the "posts" table
-    const { data, error } = await supabase
-      .from('posts')
-      .insert([{ title: title.trim() || 'Anonymous', content: newContent }]);
+    const { data: postData, error: postError } = await supabase
+      .from("posts")
+      .insert([{ title: title.trim() || "Anonymous", content: newContent }])
+      .select();
 
-    if (error) {
-      console.log('Error creating post:', error);
-    } else {
-      console.log('Post created successfully:', data);
+    if (postError) {
+      console.log("Error creating post:", postError);
+      setErrorMessage("An error occurred while creating the post.");
+      return;
     }
 
-    history('/');
+    console.log("Post created successfully:", postData);
+    console.log(postData);
+
+    // Upload the image to Supabase Storage and connect it to the post
+    const postId = postData[0]?.post_id; // add a check for existence of postData
+    if (!postId) {
+      setErrorMessage("An error occurred while creating the post.");
+      return;
+    }
+
+    if (image) {
+      const { data: imageData, error: imageError } = await supabase.storage
+        .from("images")
+        .upload(`public/${postId}.jpg`, image, {
+          cacheControl: '3600',
+          contentType: 'image/png'
+        });
+
+      if (imageError) {
+        console.log("Error uploading image:", imageError);
+        setErrorMessage("An error occurred while uploading the image.");
+        return;
+      }
+
+      console.log("Image uploaded successfully:", imageData);
+
+      // Update the post with the image URL
+      const { data: publicURL } = await supabase.storage
+        .from("images")
+        .getPublicUrl(`${imageData.path}`);
+
+      const { data: updateData, error: updateError } = await supabase
+        .from("posts")
+        .update({ image_url: `${publicURL.publicUrl}` })
+        .eq('post_id', postData[0].post_id );
+
+      if (updateError) {
+        console.log("Error updating post:", updateError);
+        setErrorMessage("An error occurred while updating the post.");
+        return;
+      }
+
+      console.log("Post updated successfully:", updateData);
+    }
+
+    history("/");
   };
 
   return (
@@ -63,6 +138,14 @@ const Post = () => {
           />
 
           <label htmlFor="content">Content:</label>
+          <input
+            type="file"
+            id="image-upload"
+            name="image-upload"
+            accept=".png,.jpg,.jpeg"
+            title="Upload image"
+            onChange={handleImageChange}
+          />
           <textarea
             id="content"
             value={content}
