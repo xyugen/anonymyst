00eba30e322433bd6efed496f1f99fa2cdf760d5
Alex Arias <alex.arias.jorym@gmail.com>
2023-03-18 21:50:23 +0800

    chore: remove unused useEffect

--- src/Components/Input/CommentBox.jsx
+++ src/Components/Input/CommentBox.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { supabase } from '../../Services/supabase'
